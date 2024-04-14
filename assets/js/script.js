e68f6d102ed144e52fdd47e3b33e96adf2a24aa2
Siyasanga Mbiko <smbiko@gmail.com>
2024-04-14 16:30:50 +0000

    redone the game html page to a new format that will be based on just questions not picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,61 @@ closeModal.addEventListener("click", () => {
 });
 
 
+
+
+
+// array of questions, where correctAnswer has value of the index of the correct answer in the array of options.
+
+let quizData = [
+  {
+    question: " What is the name of the sneaker presented on the picture?",
+    image: "../images/airjordan4-frontleft.png",
+    correctAnswerPosition: 0,
+    options: ["Jordans 14", "Jordans 11", "Jordans 1"]
+  },
+]
+
+// Select elements from the DOM
+
+const section = document.querySelector(".sneaker-showcase")
+const sneaker= document.querySelector(".sneaker");
+const body = document.querySelector("body");
+
+//Previous rotation value
+let prev = 0;
+//Calculation
+let calc = 0;
+//Drag sensitivity 
+const sensitivity =2;
+
+//Get the X Postion when the mouse is clicked down
+section.addEventListener("mousedown", function (e) {
+  //get mouse X position
+  const x = e.clientX;
+
+  /* Rotate the book on mousemove */
+  section.addEventListener("mousemove", rotate);
+  function rotate(e) {
+    calc = (e.clientX - x)/ sensitivity;
+
+    sneaker.style.transform = `
+    rotateY(${calc + prev}deg)`;
+    
+    /* Change Cursor To Grabbing Icon */
+    body.style.cursor = "grabbing";
+  }
+
+  // Save rotation value for next click event 
+  prev += calc;
+
+  //Remove the Event And Cursor Icon on Mouse Release
+  window.addEventListener("mouseup", function () {
+    //remove event listener
+    section.removeEventListener("mousemove", rotate);
+    //Reset cursor icon
+    body.style.cursor = "default";
+  });
+
+});
+
+// DOM Elements
