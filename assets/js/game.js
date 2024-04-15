8d5be0c671ec85720b794bfcc7aec10130f00474
Siyasanga Mbiko <smbiko@gmail.com>
2024-04-15 10:38:03 +0000

    Added end html functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,6 @@
+/*----- https://www.youtube.com/watch?v=f4fB9Xg2JEY ----*/
+
+
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
