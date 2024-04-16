ad808cec0143b1cb6562d94090dd711cc2afd847
Siyasanga Mbiko <smbiko@gmail.com>
2024-04-16 18:33:53 +0000

    fixing highscore link to go home on deployed website

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@
 
 
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-container'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
