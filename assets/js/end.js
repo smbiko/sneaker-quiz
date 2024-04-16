b11468481931e4fcbdeb215ae24e24a5fb7bd5a2
Siyasanga Mbiko <smbiko@gmail.com>
2024-04-16 16:46:33 +0000

    Added /end html to the end js page

--- assets/js/end.js
+++ assets/js/end.js
@@ -33,6 +33,6 @@ saveHighScore = e => {
 highScores.splice(5)
 
 localStorage.setItem('highScores', JSON.stringify(highScores))
-window.location.assign('/')
+window.location.assign('/end.html')
 
 }
