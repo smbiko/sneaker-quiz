b140bc91451047c4e3b4b9634624337f7f468f3b
Siyasanga Mbiko <smbiko@gmail.com>
2024-04-16 19:18:54 +0000

    fixing the incorrect and correct items

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@
 
 
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-container'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
